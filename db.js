0f33ea8427123527f443d109de5074158b9108fc
Himanshu Ojha <ojhaojhahimanshu@gmail.com>
2025-10-19 16:15:58 +0530

    Latest frontend changes before deployment

--- db.js
+++ db.js
@@ -1,13 +1,15 @@
-import mysql from "mysql2";
+import pkg from "pg";
+const { Pool } = pkg;
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = mysql.createConnection({
+const pool = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 db.connect((err) => {
