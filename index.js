7e72049c929aca682210306098c7aa289b19bbf8
Himanshu Ojha <ojhaojhahimanshu@gmail.com>
2025-10-19 15:47:47 +0530

    Latest frontend changes before deployment

--- index.js
+++ index.js
@@ -107,5 +107,5 @@ app.get("/jobs/filter", (req, res) => {
   });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
